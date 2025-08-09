ba610ab3c488bfae914713139fdd027bf80460db
Fabricio Varisco Oliveira <fabricio.varisco@outlook.com>
2025-08-09 19:29:03 -0300

    add express and project struct

--- openai-project/src/main.ts
+++ openai-project/src/main.ts
@@ -1,18 +1,46 @@
-import { OpenAI, } from "npm:openai";
+// import { OpenAI, } from "npm:openai";
+// import {encoding_for_model} from 'tiktoken';
+
+// const openai = new OpenAI();
+
+// const main = async (): Promise<void> => {
+//     const response = await openai.chat.completions.create({
+//       model: "gpt-4o",
+//       messages: [{
+//         role: 'user',
+//         content: "test"
+//       }]
+//     })
+//     console.log(response.choices[0].message.content)
+// }
+
+// const encodePromt = () => {
+//   const prompt = "how are you today?"
+//   const encoder = encoding_for_model('gpt-4o')
+//   const words = encoder.encode(prompt);
+//   console.log("words");
+// }
 
 
-const openai = new OpenAI();
+//  main();
 
-const main = async (): Promise<void> => {
-    const response = await openai.chat.completions.create({
-      model: "gpt-4o",
-      messages: [{
-        role: 'user',
-        content: "test"
-      }]
-    })
-    console.log(response.choices[0].message.content)
-}
+import express from "express";
 
- main();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`
+    <pre>
+           __
+          / _)
+   .-^^^-/ /
+ __/       /
+<__.|_|-|_|
+    </pre>
+    <p>Welcome to the DENOsaur API xD</p>
+  `);
+});
+app.listen(8000);
+console.log(`Server is running on http://localhost:8000`);
+
 
